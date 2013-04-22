a561c35627d86c0ddf367c00e1ed0ceeb8c6e136
Pallab Banerjee <pallabacsrdeveloper@gmail.com>
2013-04-22 20:49:17 +0530

    Search for javascript repositories
    Search for the repositories with the static keyword javascript.

--- www/js/index.js
+++ www/js/index.js
@@ -24,9 +24,22 @@ var app = {
         document.addEventListener('deviceready', this.deviceready, false);
     },
     deviceready: function() {
-        // This is an event handler function, which means the scope is the event.
-        // So, we must explicitly called `app.report()` instead of `this.report()`.
-        app.report('deviceready');
+        var xhr = new XMLHttpRequest();
+		 xhr.open('GET', 'https://api.github.com/legacy/repos/search/javascript', true);
+		  // Response handlers.
+		  xhr.onload = function () {
+			 var repos = JSON.parse(xhr.response), i, reposHTML = "";
+			 for (i = 0; i < repos.repositories.length; i++) {
+			   reposHTML += "<p><a href='https://github.com/" + repos.repositories[i].username + "/" + repos.repositories[i].name + "'>" + repos.repositories[i].name + "</a><br>" + repos.repositories[i].description + "</p>";
+			 }
+			 document.getElementById("allRepos").innerHTML = reposHTML;
+		  };
+
+		  xhr.onerror = function () {
+			 alert('error making the request.');
+		  };
+
+		xhr.send();
     },
     report: function(id) {
         // Report the event in the console
