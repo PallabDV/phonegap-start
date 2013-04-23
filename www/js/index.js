2b2941aa90eea40f8e988de7045947d450598095
Pallab Banerjee <pallabacsrdeveloper@gmail.com>
2013-04-23 16:19:54 +0530

    local Database implemented

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,25 @@
+var db;
+
 $('#reposHome').bind('pageinit', function(event) {
 	loadRepos();
+	
+	db = window.openDatabase("repodb","0.1","GitHub Repo Db", 1000);
+    db.transaction(createDb, txError, txSuccess);
 });
+
+function createDb(tx) {
+    tx.executeSql("DROP TABLE IF EXISTS repos");
+    tx.executeSql("CREATE TABLE repos(user,name)");
+}
+
+function txError(error) {
+    console.log(error);
+    console.log("Database error: " + error);
+}
+
+function txSuccess() {
+    console.log("Success");
+}
 
 $('#reposDetail').live('pageshow', function(event) {
     var owner = getUrlVars().owner;
