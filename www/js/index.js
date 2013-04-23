af2079578911dda0f875688103889fbe943d0139
Pallab Banerjee <pallabacsrdeveloper@gmail.com>
2013-04-23 14:06:57 +0530

    style changed

--- www/js/index.js
+++ www/js/index.js
@@ -29,7 +29,7 @@ var app = {
 		 xhr.open('GET', 'https://api.github.com/legacy/repos/search/javascript', true);
 		  // Response handlers.
 		  xhr.onload = function () {
-			 console && console.log(xhr.response);
+			 console && console.log("response : "+xhr.response);
 			 var repos = JSON.parse(xhr.response), i, reposHTML = "";
 			 for (i = 0; i < repos.repositories.length; i++) {
 			   reposHTML += "<p><a href='https://github.com/" + repos.repositories[i].username + "/" + repos.repositories[i].name + "'>" + repos.repositories[i].name + "</a><br>" + repos.repositories[i].description + "</p>";
