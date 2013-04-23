a106ab6b142f31dbce04e80c2324a7f677b01415
Pallab Banerjee <pallabacsrdeveloper@gmail.com>
2013-04-23 15:27:38 +0530

    JQuery mobilw UI introduced

--- www/js/index.js
+++ www/js/index.js
@@ -1,65 +1,15 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    initialize: function() {
-		console.log("App initialized");
-        this.bind();
-    },
-    bind: function() {
-		console.log("App binded");
-        document.addEventListener('deviceready', this.deviceready, false);
-    },
-    deviceready: function() {
-		console && console.log('device is ready');
-        var xhr = new XMLHttpRequest();
-		xhr.responseType = "json";
-		 xhr.open('GET', 'https://api.github.com/legacy/repos/search/javascript', true);
-		  // Response handlers.
-		  xhr.onload = function () {
-			  console.log(xhr);
-			 //console && console.log("response : "+xhr.response +" / "+xhr);
-			 var responseStr = xhr.respose || xhr.responseText,
-				repos = JSON.parse(responseStr),
-				i,
-				reposHTML = "";
-				
-			 for (i = 0; i < repos.repositories.length; i++) {
-			   reposHTML += "<p><a href='https://github.com/" + repos.repositories[i].username + "/" + repos.repositories[i].name + "'>" + repos.repositories[i].name + "</a><br>" + repos.repositories[i].description + "</p>";
-			 }
-			 document.getElementById("allRepos").innerHTML = reposHTML;
-		  };
+$('#reposHome').bind('pageinit', function(event) {
+	loadRepos();
+});
 
-		  xhr.onerror = function () {
-			 alert('error making the request.');
-		  };
-
-		xhr.send();
-    },
-    report: function(id) {
-        // Report the event in the console
-        console.log("Report: " + id);
-
-        // Toggle the state from "pending" to "complete" for the reported ID.
-        // Accomplished by adding .hide to the pending element and removing
-        // .hide from the complete element.
-        document.querySelector('#' + id + ' .pending').className += ' hide';
-        var completeElem = document.querySelector('#' + id + ' .complete');
-        completeElem.className = completeElem.className.split('hide').join('');
-    }
-};
+function loadRepos() {
+    $.ajax("https://api.github.com/legacy/repos/search/javascript").done(function(data) {
+        var i, repo;
+        $.each(data.repositories, function (i, repo) {
+            $("#allRepos").append("<li><a href='https://github.com/" + repo.username + "/" + repo.name + "'>"
+            + "<h4>" + repo.name + "</h4>"
+            + "<p>" + repo.username + "</p></a></li>");
+        });
+        $('#allRepos').listview('refresh');
+    });
+}
